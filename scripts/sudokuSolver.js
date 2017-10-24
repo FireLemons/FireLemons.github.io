c0a2ba847e299f4182a875209c4fffa6c966e616
FireLemons <ShenYang64@gmail.com>
2017-10-23 20:18:03 -0500

    dropdown functionality complete

--- scripts/sudokuSolver.js
+++ scripts/sudokuSolver.js
@@ -1,4 +1,11 @@
 angular.module('SudokuSolver', []).controller('SudokuPuzzleController', function() {
 
-	this.puzzle = 'A';
+	this.instances = ["A", "B", "C"];
+	this.puzzleInstance = "A";
+	this.puzzle=puzzles[0];
+
+	this.loadPuzzle = function(instance){
+		this.puzzle = puzzles[instance];
+		this.puzzleInstance = this.instances[instance];
+	}
 });
