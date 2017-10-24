0fa2851aa56ade349cd1e6bde5c4b1901153fec3
FireLemons <ShenYang64@gmail.com>
2017-10-24 11:09:22 -0500

    binding puzzle model to view

--- scripts/sudokuSolver.js
+++ scripts/sudokuSolver.js
@@ -2,10 +2,16 @@ angular.module('SudokuSolver', []).controller('SudokuPuzzleController', function
 
 	this.instances = ["A", "B", "C"];
 	this.puzzleInstance = "A";
-	this.puzzle=puzzles[0];
+	this.puzzle = puzzles[0].slice(0);;
 
 	this.loadPuzzle = function(instance){
-		this.puzzle = puzzles[instance];
+		this.puzzle = puzzles[instance].slice(0);
 		this.puzzleInstance = this.instances[instance];
+	}
+
+	this.getBoardValue = function(i, j, k, l){
+		var value = this.puzzle[i * 3 + k][j * 3 + l];
+
+		return (value.length == 1) ? value[0] : '';
 	}
 });
