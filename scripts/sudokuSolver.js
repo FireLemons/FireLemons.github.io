6335e787a9c1739bcbfadef8aa1cec0f684d9fa1
FireLemons <ShenYang64@gmail.com>
2017-10-24 11:58:01 -0500

    mrv heuristic added

--- scripts/sudokuSolver.js
+++ scripts/sudokuSolver.js
@@ -7,6 +7,8 @@ angular.module('SudokuSolver', []).controller('SudokuPuzzleController', function
 	this.loadPuzzle = function(instance){
 		this.puzzle = puzzles[instance].slice(0);
 		this.puzzleInstance = this.instances[instance];
+
+		console.log(this.puzzle);
 	}
 
 	this.getBoardValue = function(i, j, k, l){
@@ -19,3 +21,19 @@ angular.module('SudokuSolver', []).controller('SudokuPuzzleController', function
 		return (this.getBoardValue(i, j, k, l) === '0') ? 'invisible' : '';
 	}
 });
+
+function getNextMRV(puzzle){
+	var next = [];
+	var min = 16;
+
+	for(var i = 0; i < puzzle.length; i++){
+		for(var j = 0; j < puzzle.length; j++){
+			if(puzzle[i][j].length > 1 && puzzle[i][j].length < min){
+				min = puzzle[i][j].length;
+				next = [i, j];
+			}
+		}
+	}
+
+	return next;
+}
