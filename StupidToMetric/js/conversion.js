e45947d39921aa6208dc65e284f06e4fe37284f0
Dillon Lisk <KJ4TIP@comporium.net>
2018-07-06 16:50:53 -0500

    Added answer checking functionality

--- StupidToMetric/js/conversion.js
+++ StupidToMetric/js/conversion.js
@@ -1,13 +1,23 @@
 var convertToMetric = new Vue({
-	el: '#conversionInput',
-	data: {
-		given: Math.ceil(Math.random() * 50),
-		answer: 0
-	},
-	methods: {
-		checkAnswer: function(){
-			this.given;
-			console.log("432423423");
-		}
+    el: '#conversionInput',
+    data: {
+	tolerance: 0.1,
+	score: 0,
+	given: Math.ceil(Math.random() * 50),
+	answer: "",
+	answerClass: "lightGray"
+    },
+    methods: {
+	checkAnswer: function(){
+	    var correct = this.given/2.54;
+	    if(Math.abs(correct - this.answer)/correct <= this.tolerance) {
+		this.answerClass = "lightGray correct";
+		this.given = Math.ceil(Math.random() * 50);
+		score += 1;
+	    }
+	    else {
+		this.answerClass = "lightGray wrong";
+	    }
 	}
+    }
 });
