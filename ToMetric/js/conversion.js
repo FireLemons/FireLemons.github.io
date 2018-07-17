b3798d53eebcb393f9cde80a1976ae77a5b98771
FireLemons <shenyang64@gmail.com>
2018-07-17 02:23:13 -0500

    options page created, replaced tabs with spaces

--- ToMetric/js/conversion.js
+++ ToMetric/js/conversion.js
@@ -1,29 +1,29 @@
 var convertToMetric = new Vue({
     el: '#conversionInput',
     data: {
-		tolerance: 0.1,
-		score: 0,
-		given: Math.ceil(Math.random() * 50),
-		answer: "",
-		answerClass: "grey darken-3"
+        tolerance: 0.1,
+        score: 0,
+        given: Math.ceil(Math.random() * 50),
+        answer: "",
+        answerClass: "grey darken-3"
     },
     methods: {
-		checkAnswer: function(){
-			var correct = this.given / 2.54;
-			
-			if(Math.abs(correct - this.answer) / correct <= this.tolerance) {
-				this.given = Math.ceil(Math.random() * 50);
-				this.score += 1;
-				
-				M.toast({
-					html: "What a great job you did!!!"
-				});
-				
-				this.answerClass = "grey darken-3";
-				this.answer = "";
-			} else {
-				this.answerClass = "grey darken-3 wrong";
-			}
-		}
+        checkAnswer: function(){
+            var correct = this.given / 2.54;
+            
+            if(Math.abs(correct - this.answer) / correct <= this.tolerance) {
+                this.given = Math.ceil(Math.random() * 50);
+                this.score += 1;
+                
+                M.toast({
+                    html: "What a great job you did!!!"
+                });
+                
+                this.answerClass = "grey darken-3";
+                this.answer = "";
+            } else {
+                this.answerClass = "grey darken-3 wrong";
+            }
+        }
     }
 });
--- ToMetric/js/nav.js
+++ ToMetric/js/nav.js
@@ -1,17 +1,17 @@
 Vue.component("site-nav", {
-	template: `
-		<header>
-			<nav id="nav" class="grey darken-3">
-				<div class="nav-wrapper">
-					<a href="./" class="brand-logo">ToMetric</a>
-					<ul class="right">
-						<li><a class="" href="options.html"><i class="material-icons left">settings</i>Options</a></li>
-					</ul>
-				</div>
-			</nav>
-		</header>`
+    template: `
+        <header>
+            <nav id="nav" class="grey darken-3">
+                <div class="nav-wrapper">
+                    <a href="./" class="brand-logo">ToMetric</a>
+                    <ul class="right">
+                        <li><a class="" href="options.html"><i class="material-icons left">settings</i>Options</a></li>
+                    </ul>
+                </div>
+            </nav>
+        </header>`
 });
 
 new Vue({
-	el: '#navBar'
+    el: '#navBar'
 });
