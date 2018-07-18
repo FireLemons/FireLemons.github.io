76258f4922e1b5097a293e327e07f63a3400cad3
Dillon Lisk <KJ4TIP@comporium.net>
2018-07-18 16:09:51 -0500

    Wrap options data in options object

--- ToMetric/js/options.js
+++ ToMetric/js/options.js
@@ -1,67 +1,69 @@
 var options = new Vue({
     el: "#options",
     data: {
-        conversions: {
-            distance: {
-                enabled: true,
-                customary: {
-                    inches: true,
-                    feet: true,
-                    yards: true,
-                    miles: true
-                },
-                metric: {
-                    millimeters: true,
-                    centimeters: true,
-                    meters: true,
-                    kilometers: true
-                }
-            },
-            liquid_volume: {
-                enabled: true,
-                customary: {
-                    gallons: true,
-                    quarts: true,
-                    pints: true,
-                    fluid_ounces: true
-                },
-                metric: {
-                    milliliters: true,
-                    liters: true
-                }
-            },
-            mass: {
-                enabled: true,
-                customary: {
-                    ounces: true,
-                    pound: true,
-                    ton_imperial: true
-                },
-                metric: {
-                    grams: true,
-                    kilograms: true,
-                    ton_metric: true
-                }
-            },
-            speed: {
-                enabled: true,
-                customary: {
-                    miles_per_hour: true
-                },
-                metric: {
-                    kilometers_per_hour: true
-                }
-            },
-            temperature: {
-                enabled: true,
-                customary: {
-                    fahrenheit: true
-                },
-                metric: {
-                    celsius: true
-                }
+	options: {
+            conversions: {
+		distance: {
+                    enabled: true,
+                    customary: {
+			inches: true,
+			feet: true,
+			yards: true,
+			miles: true
+                    },
+                    metric: {
+			millimeters: true,
+			centimeters: true,
+			meters: true,
+			kilometers: true
+                    }
+		},
+		liquid_volume: {
+                    enabled: true,
+                    customary: {
+			gallons: true,
+			quarts: true,
+			pints: true,
+			fluid_ounces: true
+                    },
+                    metric: {
+			milliliters: true,
+			liters: true
+                    }
+		},
+		mass: {
+                    enabled: true,
+                    customary: {
+			ounces: true,
+			pound: true,
+			ton_imperial: true
+                    },
+                    metric: {
+			grams: true,
+			kilograms: true,
+			ton_metric: true
+                    }
+		},
+		speed: {
+                    enabled: true,
+                    customary: {
+			miles_per_hour: true
+                    },
+                    metric: {
+			kilometers_per_hour: true
+                    }
+		},
+		temperature: {
+                    enabled: true,
+                    customary: {
+			fahrenheit: true
+                    },
+                    metric: {
+			celsius: true
+                    }
+		}
             }
-        }
+	}
     },
     methods: {
         loadOptions: function(){
