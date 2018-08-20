15065854a560abb23411c3051eee32f46191009a
FireLemons <shenyang64@gmail.com>
2018-08-19 19:30:16 -0500

    added angular mock dependency, testing enabled check changed to test

--- SudokuSolver/scripts/sudokuTests.js
+++ SudokuSolver/scripts/sudokuTests.js
@@ -1,3 +1,20 @@
-if(typeof testing === 'undefined' || !testing){
-    throw 'Testing Not Enabled';
-}
+describe('Check if testing is enabled', function(){
+    it('should be enabled', function(){
+        expect(testing).toBeDefined();
+        expect(testing).toBe(true);
+    });
+});
+
+describe('Initializing the puzzle', function(){
+    var sudokuPuzzleService;
+    
+    beforeEach(module('SudokuSolver'));
+    beforeEach(inject(function(_sudokuPuzzle_){
+        sudokuPuzzleService = _sudokuPuzzle_;
+        console.log(sudokuPuzzleService);
+    }));
+    
+    it('', function(){
+        expect(true).toEqual(true);
+    });
+});
