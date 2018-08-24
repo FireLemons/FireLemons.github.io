6a75befd602836a8a3920105729c2ecb7224f169
FireLemons <shenyang64@gmail.com>
2018-08-24 02:57:45 -0500

    added some blank suites and some tests for getSubGridConstrained function

--- SudokuSolver/scripts/sudokuTests.js
+++ SudokuSolver/scripts/sudokuTests.js
@@ -16,6 +16,41 @@ afterEach(function(){
     sudokuPuzzleService = undefined;
 });
 
+describe('The getSubGridConstrained function', function(){
+    it('returns a set of 4 coordiantes given coordinates to a box on the board', function(){
+        for(var i = 0; i < 9; i++){
+            for(var j = 0; j < 9; j++){
+                expect(sudokuPuzzleService.getSubGridConstrained(i, j).length).toBe(4);
+            }
+        }
+    });
+    
+    it('doesn\'t return a set of coordinates interstecting with parameters row i or column j', function(){
+        for(var i = 0; i < 9; i++){
+            for(var j = 0; j < 9; j++){
+                var coordinates = sudokuPuzzleService.getSubGridConstrained(i, j),
+                    columnIndicies = coordinates.map(function(coords){
+                        return coords[1];
+                    }),
+                    rowIndicies = coordinates.map(function(coords){
+                        return coords[0];
+                    });
+                
+                expect(rowIndicies).not.toContain(i);
+                expect(columnIndicies).not.toContain(j);
+            }
+        }
+    });
+});
+
+describe('The getConstrined function', function(){
+    
+});
+
+describe('The heuristic function', function(){
+    
+});
+
 describe('Initializing the puzzle object', function(){
     
     it('makes a 2D 9x9 matrix of Box objects on initialization with no args', function(){
